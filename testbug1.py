bd98ae7d713ac934e76b05d69903aa647d94233e
sunjiajian <1003512683@qq.com>
2015-12-08 18:47:35 +0800

    update

--- testbug1.py
+++ testbug1.py
@@ -2,11 +2,13 @@ import urllib.request
 import json
 import time
 import datetime
+import tushare
 
-codes = {'SZ000002': '02', 'SH000001': 'big bro'}
+codes = {'SZ000002': '02', 'SZ000996': 'zq', 'SH000001': 'big bro'}
 
 while 1:
     time.sleep(5)
+    print('------------------------------')
     print(datetime.datetime.now().strftime("%H:%M:%S"))
     for code, name in codes.items():
         he1 = {'X-Requested-With': 'XMLHttpRequest',
@@ -15,12 +17,12 @@ while 1:
                 'Host': 'xueqiu.com',
                 #'Connection':'keep-alive',
                 #'Accept':'*/*',
-                'cookie':'s=2dq712k3rx; xq_a_token=104d73a454a6ff79118019ac5a2722428ded10fb; xq_r_token=6a9d2d40660852a677475c3e23df8a04bb8284ce; Hm_lvt_1db88642e346389874251b5a1eded6e3=1449479272; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1449479909; __utmt=1; __utma=1.1802555965.1449479272.1449479272.1449479272.1; __utmb=1.4.10.1449479272; __utmc=1; __utmz=1.1449479272.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)'
+                'cookie':'s=2dq712k3rx; xq_a_token=b091de3cd9721c7dd67d4115f609f42a95a6200a; xq_r_token=97ad548663a06fad5d7e0f733f67a89797d1d945; __utmt=1; __utma=1.1802555965.1449479272.1449479272.1449540187.2; __utmb=1.4.10.1449540187; __utmc=1; __utmz=1.1449479272.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); Hm_lvt_1db88642e346389874251b5a1eded6e3=1449479272,1449540187; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1449541963'
                }
         url = "http://xueqiu.com/v4/stock/quote.json?code=%s&_=%s" % (code, int(time.time()))
         req = urllib.request.Request(url, headers=he1)
         data = urllib.request.urlopen(req).read().decode('UTF-8')
         js_data = json.loads(data)
         print(js_data[code]['current'], '\t', end='')
-        print(js_data[code]['percentage'], '\t', end='')
+        print(js_data[code]['percentage'], '\t\t', end='')
         print(name)
