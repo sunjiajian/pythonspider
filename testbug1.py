fee6283bedeeee4ac995e8bc5b4db5d51b489aa0
sunjiajian <1003512683@qq.com>
2015-12-30 13:07:12 +0800

    update

--- testbug1.py
+++ testbug1.py
@@ -5,32 +5,65 @@ import datetime
 import tushare as ts
 import pandas
 
-days = {'day0': '2015-12-09', 'day1': '2015-12-10'}
+def code2str(cd):
+    if cd < 10:
+        scd = '00000%d' % cd
+        return scd
+    elif cd < 100:
+        scd = '0000%d' % cd
+        return scd
+    elif cd < 1000:
+        scd = '000%d' % cd
+        return scd
+    elif cd < 10000:
+        scd = '00%d' % cd
+        return scd
+    elif cd < 100000:
+        scd = '%d' % cd
+        return scd
+    else:
+        scd = str(cd)
+        return scd
 
-dfall = ts.get_today_all()
-dfall.to_excel('all.xlsx')
+days = {'day0': '2015-12-28', 'day1': '2015-12-29'}
 
-lhb = ts.top_list(days['day0'])
-# lhb.to_excel('test1.xlsx')
-print(len(lhb.index), len(lhb.columns))
+# dfall = ts.get_today_all()
+# dfall.to_excel('all.xlsx')
+dfall = pandas.read_excel('all.xlsx')
 
-sub = 0
-
-n_data = pandas.DataFrame(columns=('n_open', 'n_close', 'n_high', 'n_low', 'n_p_change', 'n_turnover'))
-
-for code in lhb.code:
+for code in dfall.code:
     try:
-        data = ts.get_hist_data(code, days['day1'], days['day1'])
-        n_data.loc[sub] = [data['open'][0], data['close'][0], data['high'][0], data['low'][0], data['p_change'][0], data['turnover'][0]]
-        # print(n_data)
+        cd = code2str(code)
+        hist = ts.get_hist_data(cd, '2015-12-28', '2015-12-29')
+        mnd0 = float(hist['ma5'][0]) - float(hist['ma20'][0])
+        mnd1 = float(hist['ma5'][1]) - float(hist['ma20'][1])
+        if mnd0 < mnd1:
+            print(cd, '----------------------------------------------------')
+            # print(hist)
     except:
-        n_data.loc[sub] = ['nan', 'nan', 'nan', 'nan', 'nan', 'nan']
-    sub += 1
-    print(sub)
+        pass
 
-lhb = lhb.merge(n_data, left_index=True, right_index=True)
-print(lhb)
-lhb.to_excel('test1.xlsx')
+# lhb = ts.top_list(days['day0'])
+# lhb.to_excel('test1.xlsx')
+# print(len(lhb.index), len(lhb.columns))
+
+# sub = 0
+
+# n_data = pandas.DataFrame(columns=('n_open', 'n_close', 'n_high', 'n_low', 'n_p_change', 'n_turnover'))
+
+# for code in lhb.code:
+#     try:
+#         data = ts.get_hist_data(code, days['day1'], days['day1'])
+#         n_data.loc[sub] = [data['open'][0], data['close'][0], data['high'][0], data['low'][0], data['p_change'][0], data['turnover'][0]]
+#         # print(n_data)
+#     except:
+#         n_data.loc[sub] = ['nan', 'nan', 'nan', 'nan', 'nan', 'nan']
+#     sub += 1
+#     print(sub)
+
+# lhb = lhb.merge(n_data, left_index=True, right_index=True)
+# print(lhb)
+# lhb.to_excel('test1.xlsx')
 
 
 # while 1:
