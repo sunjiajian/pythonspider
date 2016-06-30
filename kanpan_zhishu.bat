f90dcf4448f266ad0abda6c3099f821769b69a40
sunjiajian <1003512683@qq.com>
2016-06-30 14:37:51 +0800

    update

---
+++ kanpan_zhishu.bat
@@ -0,0 +1,2 @@
+python kanpan_zhishu.py
+pause
---
+++ kanpan_zhishu.py
@@ -0,0 +1,27 @@
+import urllib.request
+import json
+import time
+import datetime
+import tushare
+import pandas
+
+codes = {'01_SH': 'sh', '02_SZ': 'sz', '03_CYB': 'cyb', '10_HLWB': '150195', '11_WJRB': '150332'}
+
+while 1:
+    time.sleep(3)
+    sub = 0
+    codesdf = pandas.DataFrame(columns=('name', 'price', 'percent', 'amount'))
+    for name, code in codes.items():
+        try:
+            df = tushare.get_realtime_quotes(code)
+            percent = 100.0 * (float(df['price'][0]) - float(df['pre_close'][0]))/float(df['pre_close'][0])
+            amount = float(df['amount'][0]) / 100000000
+            # if float(df['amount'][0]) < 100000000:
+            #     amount = float(df['amount'][0]) / 10000
+            codesdf.loc[sub] = [name, df['price'][0], percent, amount]
+            sub += 1
+        except:
+            pass
+    print('------------------------------')
+    print(datetime.datetime.now().strftime("%H:%M:%S"))
+    print(codesdf.sort_values(by='name'))
